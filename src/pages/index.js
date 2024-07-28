3ecbfdbd81a53451991bf208f34c547f27c07dbe
Pratham <agrawalpratham2002@gmail.com>
2024-07-28 15:22:38 +0530

    added manifest.json & icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,9 +13,91 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Pratham Agrawal - Portfolio</title>
+        <title>Pratham Agrawal | Full Stack Web Developer</title>
         <meta name="description" content="Pratham Agrawal's portfolio" />
-        <link rel="icon" href="/assets/png/personal_logo.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#d3f4f7" />
+        <meta name="msapplication-TileColor" content="#d3f4f7" />
+        <meta name="theme-color" content="#d3f4f7" />{" "}
+        <meta
+          name="description"
+          content="Pratham Agrawal, a Full Stack Web Developer with expertise in building dynamic web applications and platforms. Explore my professional journey, projects, and skills in technologies like Next.js, ReactJS, Node.js, Firebase, AWS, and more. Discover how I lead the development of restaurant discovery platforms, LMS portals, and versatile web applications."
+        />
+        <meta
+          name="google-site-verification"
+          content="xLgsNqKuFWO2leEq61qdWwQyEJutNxKKEZQX2alS95U"
+        />
+        <meta
+          name="keywords"
+          content="Pratham Agrawal, Full Stack Web Developer, Web Development, Next.js, ReactJS, Node.js, Firebase, AWS, Restaurant Discovery Platform, Learning Management System, LMS, Web Applications, UI Design, Software Engineer, Krishworks Technology and Research Labs Pvt. Ltd."
+        />
+        <meta name="author" content="Pratham Agrawal" />
+        <link rel="canonical" href="https://agrawalpratham.in/" />
+        <meta
+          property="og:title"
+          content="Pratham Agrawal | Full Stack Web Developer"
+        />
+        <meta
+          property="og:description"
+          content="Pratham Agrawal, a Full Stack Web Developer with expertise in building dynamic web applications and platforms. Explore my professional journey, projects, and skills in technologies like Next.js, ReactJS, Node.js, Firebase, AWS, and more. Discover how I lead the development of restaurant discovery platforms, LMS portals, and versatile web applications."
+        />
+        <meta
+          property="og:image"
+          content="https://agrawalpratham.in/assets/png/me.jpg"
+        />
+        <meta property="og:url" content="https://agrawalpratham.in/" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@agrawal_2002" />
+        <meta name="twitter:creator" content="@agrawal_2002" />
+        <meta
+          name="twitter:title"
+          content="Pratham Agrawal | Full Stack Web Developer"
+        />
+        <meta
+          name="twitter:description"
+          content="Pratham Agrawal, a Full Stack Web Developer with expertise in building dynamic web applications and platforms. Explore my professional journey, projects, and skills in technologies like Next.js, ReactJS, Node.js, Firebase, AWS, and more. Discover how I lead the development of restaurant discovery platforms, LMS portals, and versatile web applications."
+        />
+        <meta
+          name="twitter:image"
+          content="https://agrawalpratham.in/assets/png/me.jpg"
+        />
+        {/* <script type="application/ld+json">
+      {
+        "@context": "https://schema.org",
+        "@type": "Person",
+        "name": "Pratham Agrawal",
+        "url": "https://agrawalpratham.in",
+        "sameAs": [
+          "https://www.linkedin.com/in/agrawalpratham/",
+          "https://github.com/agrawal-pratham",
+          "https://www.instagram.com/agrawal___pratham/",
+          "https://twitter.com/agrawal_2002",
+          "http://blogs.agrawalpratham.in/",
+          "https://solutions.agrawalpratham.in/"
+        ],
+        "jobTitle": "Full Stack Web Developer",
+        "description": "Pratham Agrawal, an aspiring Full Stack Web Developer, is passionate about creating interactive web applications and experiences.",
+        "image": "https://agrawalpratham.in/assets/images/me_latest.jpg"
+      }
+    </script> */}
       </Head>
 
       <Header />
